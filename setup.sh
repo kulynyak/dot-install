9459d6dbedb00c1bd2ca672f7c20a189a0657013
Andriy Kulynyak <kulynyak@gmail.com>
2016-04-01 14:02:15 +0300

    Installation improvements

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+if [ $(uname) = "Darwin" ] ; then
+  if ! type brew > /dev/null ; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+  if ! type git > /dev/null ; then
+    brew install git
+  fi
+fi
+
 export PATH=~/.ellipsis/bin:$PATH
 curl -sL ellipsis.sh | sh
 
